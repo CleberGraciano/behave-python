2fe19b265e6a92217cb555de00874ab4ba8e5730
cleberGraciano <cleber_batista2010@hotmail.com>
2023-04-26 18:23:06 -0300

    Subindo aplicação

---
+++ steps/google_steps.py
@@ -0,0 +1,24 @@
+from behave import *
+from nose.tools import assert_equal
+from pages.google_page import GooglePage
+
+googlePage = GooglePage()
+
+@given(u'que acesso a página do Google')
+def step_impl(context):
+    googlePage.acess_page('https://www.google.com.br/')
+
+
+@given(u'que preencho o campo de pesquisa com Python')
+def step_impl(context):
+    googlePage.send_keys_input_pesquisa('Python')
+
+
+@when(u'clico no botão de pesquisar')
+def step_impl(context):
+    googlePage.click_button_pesquisar()
+
+
+@then(u'devo visualizar os resultados')
+def step_impl(context):
+    assert_equal(googlePage.get_text_title_resultado(), 'Python')
